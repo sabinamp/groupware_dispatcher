09472925a9a439d31a4b4cf4dad587da79e713bf
Anastasia <anastasia.tsypkaykina@students.fhnw.ch>
2020-07-01 00:27:12 +0200

    Started a branch

--- src/main/java/groupware/dispatcher/AppStarter.java
+++ src/main/java/groupware/dispatcher/AppStarter.java
@@ -5,7 +5,7 @@ import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+import javafx.stage.Stage;
 public class AppStarter extends Application {
 
     @Override
