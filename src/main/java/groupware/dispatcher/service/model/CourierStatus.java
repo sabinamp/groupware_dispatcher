21f63e86733ce8932516baf0d800940b01480287
Sabina Borbely <sabina.borbely@students.fhnw.ch>
2020-07-17 18:12:39 +0200

    updated the string value of the enums Conn and CourierStatus to ignore case

--- src/main/java/groupware/dispatcher/service/model/CourierStatus.java
+++ src/main/java/groupware/dispatcher/service/model/CourierStatus.java
@@ -17,7 +17,7 @@ public enum CourierStatus {
     NOT_AVAILABLE_BREAK("Not available. Having a break"),
 
 
-    NOT_AVAILABLE_OUT_OF_BATTERY("Not available. Running out of battery.");
+    NOT_AVAILABLE_OUT_OF_BATTERY("Not available. Out of battery.");
 
     private String value;
 
@@ -34,7 +34,7 @@ public enum CourierStatus {
     @JsonCreator
     public static CourierStatus fromValue(String text) {
         for (CourierStatus b : CourierStatus.values()) {
-            if (String.valueOf(b.value).equals(text)) {
+            if (String.valueOf(b.value).equalsIgnoreCase(text)) {
                 return b;
             }
         }
