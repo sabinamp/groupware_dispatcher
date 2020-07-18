4f3df2ab922755d57e36e36a10698e6aab78399b
Sabina Borbely <sabina.borbely@students.fhnw.ch>
2020-07-18 19:12:32 +0200

    using BorderPane and a clock demo at the bottom of the BorderPane

--- src/main/java/groupware/dispatcher/AppStarter.java
+++ src/main/java/groupware/dispatcher/AppStarter.java
@@ -2,26 +2,59 @@ package groupware.dispatcher;
 
 import groupware.dispatcher.service.mqtt.BrokerConnection;
 import groupware.dispatcher.view.ApplicationUI;
+import groupware.dispatcher.view.MainHeader;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 public class AppStarter extends Application {
 
+    private volatile boolean enough = false;
+    private final Text txtTime = new Text();
+    // this is timer thread which will update out time view every second
+    Thread timer = new Thread(() -> {
+        SimpleDateFormat dt = new SimpleDateFormat("hh:mm:ss");
+        while(!enough) {
+            try {
+                // running "long" operation not on UI thread
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+            final String time = dt.format(new Date());
+            Platform.runLater(()-> {
+                // updating live UI object requires JavaFX App Thread
+                txtTime.setText(time);
+            });
+        }
+    });
     @Override
     public void start(Stage primaryStage){
-        Parent   rootPanel = new ApplicationUI();
-
-        Scene scene = new Scene(rootPanel);
+        ApplicationUI rootPanel = new ApplicationUI();
+        rootPanel.addClockToHeader(txtTime);
+        Scene scene = new Scene(rootPanel, 800,500);
 
         primaryStage.setTitle("Dispatcher GUI");
         BrokerConnection connection= new BrokerConnection();
         primaryStage.setScene(scene);
+        timer.start();
         primaryStage.show();
 
     }
-
+    @Override
+    public void stop() {
+        // we need to stop our working thread after closing a window
+        // or our program will not exit
+        enough = true;
+    }
     public static void main(String[] args) {
         launch(args);
     }
--- src/main/java/groupware/dispatcher/view/ApplicationUI.java
+++ src/main/java/groupware/dispatcher/view/ApplicationUI.java
@@ -1,13 +1,16 @@
 package groupware.dispatcher.view;
 
 import groupware.dispatcher.view.util.ViewMixin;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.VBox;
-public class ApplicationUI extends VBox implements ViewMixin {
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 
-        private Node header;
+public class ApplicationUI extends BorderPane implements ViewMixin {
+
+        public MainHeader header;
 
         public ApplicationUI() {
                 init();
@@ -22,29 +25,29 @@ public class ApplicationUI extends VBox implements ViewMixin {
         @Override
         public void initializeParts() {
                 header = new MainHeader();
+
         }
 
         @Override
         public void layoutParts() {
 
-        HBox hb1= new HBox();
-        hb1.setSpacing(10);
+                this.setRight(new Text("Right "));
+                this.setCenter(new Text("Center"));
+                this.setBottom(new Text(" Bottom"));
+                this.setLeft(new Text(" Left"));
 
-        VBox vbLeft= new VBox();
-        vbLeft.setSpacing(10);
-        vbLeft.getChildren().add(new CourierListView());
+                //to be added: the orders view and the detailed order view
+                this.setTop(header);
+                BorderPane.setAlignment(header, Pos.CENTER);
 
-        vbLeft.setPrefSize(550,700);
-        VBox vboxOnTheRight= new VBox();
-        vboxOnTheRight.setPrefSize(550,700);
-
-        hb1.getChildren().addAll(vbLeft,vboxOnTheRight);
-        getChildren().addAll(header, hb1);
-        setVgrow(hb1, Priority.ALWAYS);
         }
 
         @Override
         public void setupBindings() {
 
+        }
+
+        public void addClockToHeader(Text txtTimer){
+                header.addClockDemoToHeader(txtTimer);
         }
 }
