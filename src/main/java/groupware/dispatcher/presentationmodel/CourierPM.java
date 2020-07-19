012ed12ad7877ca3290d5dff40b8576fb11d45d9
Sabina Borbely <sabina.borbely@students.fhnw.ch>
2020-07-19 07:52:42 +0200

    updated CourierPM

--- src/main/java/groupware/dispatcher/presentationmodel/CourierPM.java
+++ src/main/java/groupware/dispatcher/presentationmodel/CourierPM.java
@@ -1,17 +1,50 @@
 package groupware.dispatcher.presentationmodel;
 
+import groupware.dispatcher.service.model.Conn;
 import groupware.dispatcher.service.model.Courier;
 import groupware.dispatcher.service.model.CourierStatus;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
 public class CourierPM {
     private String courierId;
-    private String courierName;
-    private CourierStatus courierStatus;
+    private static final String ELLIPSIS = "...";
+    private final StringProperty name = new SimpleStringProperty(ELLIPSIS);
 
+    private final ObjectProperty<CourierStatus> courierStatus = new SimpleObjectProperty<>();
+    private final ObjectProperty<Conn> courierConnectionStatus = new SimpleObjectProperty<>();
 
     public CourierPM(Courier courier){
         courierId= courier.getCourierId();
-        courierName = courier.getCourierInfo().getCourierName();
-        courierStatus = courier.getCourierInfo().getStatus();
+        setName(courier.getCourierInfo().getCourierName());
+        setCourierStatus(courier.getCourierInfo().getStatus());
+    }
+
+    public CourierStatus getCourierStatus() {
+        return courierStatus.get();
+    }
+
+    public ObjectProperty<CourierStatus> courierStatusProperty() {
+        return courierStatus;
+    }
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public void setCourierStatus(CourierStatus courierStatus) {
+        this.courierStatus.set(courierStatus);
+    }
+
+    public Conn getCourierConnectionStatus() {
+        return courierConnectionStatus.get();
+    }
+
+    public ObjectProperty<Conn> courierConnectionStatusProperty() {
+        return courierConnectionStatus;
+    }
+    public void setCourierConnectionStatus(Conn courierConnectionStatus) {
+        this.courierConnectionStatus.set(courierConnectionStatus);
     }
 }
